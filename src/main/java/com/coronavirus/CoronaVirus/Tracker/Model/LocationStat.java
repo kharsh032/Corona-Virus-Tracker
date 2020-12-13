05fca5ca0fe6b2bffc1ed123229e9008d90c2ad1
Harsh <kharsh032@gmail.com>
2020-12-13 20:50:28 +0530

    Add Model Class

---
+++ src/main/java/com/coronavirus/CoronaVirus/Tracker/Model/LocationStat.java
@@ -0,0 +1,10 @@
+package com.coronavirus.CoronaVirus.Tracker.Model;
+
+public class LocationStat {
+    private String state;
+    private String country;
+    private int latestTotalCases;
+    private int recoveredCases;
+    private int currentActiveCases;
+    private int deathCases;
+}
