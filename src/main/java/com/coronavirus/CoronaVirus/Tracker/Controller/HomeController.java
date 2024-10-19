04de7c5fcfd18e75e9a02f046c3710a6f928cee5
Harsh <kharsh032@gmail.com>
2024-10-19 13:26:08 +0530

    modifications in controller

--- src/main/java/com/coronavirus/CoronaVirus/Tracker/Controller/HomeController.java
+++ src/main/java/com/coronavirus/CoronaVirus/Tracker/Controller/HomeController.java
@@ -2,34 +2,34 @@ package com.coronavirus.CoronaVirus.Tracker.Controller;
 
 import com.coronavirus.CoronaVirus.Tracker.Model.LocationStat;
 import com.coronavirus.CoronaVirus.Tracker.Service.CoronaVirusDataService;
-import com.coronavirus.CoronaVirus.Tracker.constants.AppConstants;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @Controller
 @Slf4j
-@RequestMapping(value = "/coronavirus")
+@RequestMapping("/coronavirus") 
 public class HomeController {
 
     @Autowired
     CoronaVirusDataService coronaVirusDataService;
 
     @GetMapping("/")
-    public String getAllStatsForCoronaVirus(Model model)
-    {
+    public String home(Model model) { 
         List<LocationStat> allStats = coronaVirusDataService.getAllStats();
-        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getCurrentActiveCases()).sum();
-        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
-        model.addAttribute(AppConstants.LOCATION_STATS, allStats);
-        model.addAttribute(AppConstants.TOTAL_REPORTED_CASES, totalReportedCases);
-        model.addAttribute(AppConstants.TOTAL_NEW_CASES, totalNewCases);
-        log.info("Total Reported Cases = {}",totalReportedCases);
-        return "Data fetched successfully";
+        int totalReportedCases = allStats.stream().mapToInt(LocationStat::getCurrentActiveCases).sum();
+        int totalNewCases = allStats.stream().mapToInt(LocationStat::getLatestTotalCases).sum();
+
+        model.addAttribute("locationStats", allStats); 
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases); 
+
+        log.info("Total Reported Cases = {}", totalReportedCases);
+        return "home"; // Assuming you have a "home.html" template
     }
 }
